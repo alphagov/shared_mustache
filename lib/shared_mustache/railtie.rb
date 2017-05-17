6d91fec181b09ee37c9096efc622f16912041a5d
Murray Steele <muz@h-lame.com>
2017-05-17 15:17:32 +0100

    Add initializer to add hogan to precompile
    Using `render_mustache_templates` will add our hogan.js to the app via a
    script tag.  In newer versions of rails using newer versions of
    sprockets-rails we must add the hogan.js to assets.precompile or rails
    will complain and not load it.

--- lib/shared_mustache/railtie.rb
+++ lib/shared_mustache/railtie.rb
@@ -5,6 +5,11 @@ module SharedMustache
     initializer "shared_mustache.view_helpers" do
       ActionView::Base.send :include, ViewHelpers
     end
+    initializer "shared_mustache.assets.precompile" do |app|
+      app.config.assets.precompile += %w(
+        hogan-2.0.0-hotfix.js
+      )
+    end
     rake_tasks do
       Dir[File.expand_path('../tasks/*.rake', File.dirname(__FILE__))].each { |f| load f }
     end
